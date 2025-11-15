ae19b43741dc29e1c2069b6eac1be6d307c13570
Eng Natasha <tashabanks271@gmail.com>
2025-11-15 15:11:02 +0300

    Revert "Riyan frontend"

--- backend/models/follow.py
+++
@@ -1,8 +0,0 @@
-from . import db
-
-followers = db.Table(
-    'followers',
-    db.Column('follower_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
-    db.Column('followed_id', db.Integer, db.ForeignKey('users.id'), primary_key=True)
-)
-
--- backend/models/post_hashtag.py
+++
@@ -1,8 +0,0 @@
-from . import db
-
-post_hashtags = db.Table(
-    'post_hashtags',
-    db.Column('post_id', db.Integer, db.ForeignKey('posts.id'), primary_key=True),
-    db.Column('hashtag_id', db.Integer, db.ForeignKey('hashtags.id'), primary_key=True)
-)
-
