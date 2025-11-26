cf558e8c0b3d3a34d7f0668ce8f1e326c9933860
MatthewTN18 <matthewtndirangu@gmail.com>
2025-11-26 18:21:51 +0300

    Update db configuration

--- backend/config.py
+++ backend/config.py
@@ -5,6 +5,6 @@ class Config:
     # Updated connection string
     SQLALCHEMY_DATABASE_URI = os.getenv(
         "DATABASE_URL",
-        "postgresql://postgres:Maina@localhost:5432/thoughts_db" 
+        "postgresql://thoughts_user:9090@localhost:5432/thoughts_db" 
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
